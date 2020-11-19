8498877fb2a909925ae775cdd4dab5536076359c
luispcunha <luispplmcunha@gmail.com>
2020-11-19 22:39:22 +0000

    Add package to parse command line args

--- src/ampl2fy.ts
+++ src/ampl2fy.ts
@@ -1,13 +1,18 @@
 import axios from 'axios';
+import program from 'commander';
 import Scraper from './scraper/Scraper';
 
-const url = 'https://music.apple.com/us/playlist/radiohead-influences/pl.1631c4a4658f43b88cc409c4ebb9c8fb';
+(async () => {
+  program
+    .version('1.0.0')
+    .option('-pl, --playlist <url>', 'Apple Music playlist URL');
 
-async function getData() {
+  program.parse(process.argv);
+
+  const url = program.playlist;
+
   const response = await axios.get(url);
   const html: string = response.data;
 
   console.log(Scraper.getPlaylist(html));
-}
-
-getData();
+})();
