54436ff418e5affa30ba5dc0ee5c1a3e192fca73
luispcunha <luispplmcunha@gmail.com>
2020-11-20 19:11:32 +0000

    Refactor authentication

--- src/ampl2fy.ts
+++ src/ampl2fy.ts
@@ -2,20 +2,45 @@ import axios from 'axios';
 import program from 'commander';
 import dotenv from 'dotenv';
 import Scraper from './scraper/Scraper';
+import { auth } from './auth/auth';
 
-(async () => {
-  dotenv.config();
+dotenv.config();
 
-  program
-    .version('1.0.0')
-    .option('-pl, --playlist <url>', 'Apple Music playlist URL');
+const clientId = process.env.CLIENT_ID || null;
+const clientSecret = process.env.CLIENT_SECRET || null;
+const port = process.env.PORT || null;
 
-  program.parse(process.argv);
+if (clientId === null) {
+  console.log('Please specify a CLIENT_ID environment variable in the .env file');
+  process.exit(-1);
+}
 
-  const url = program.playlist;
+if (clientSecret === null) {
+  console.log('Please specify a CLIENT_KEY environment variable in the .env file');
+  process.exit(-1);
+}
+
+if (port === null) {
+  console.log('Please specify a PORT environment variable in the .env file');
+  process.exit(-1);
+}
+
+program
+  .version('1.0.0')
+  .option('-pl, --playlist <url>', 'Apple Music playlist URL');
+program.parse(process.argv);
+
+const url = program.playlist;
+
+auth(clientId, clientSecret, port, async (tokens) => {
+  console.log(tokens);
+
+  if (url === undefined) {
+    return;
+  }
 
   const response = await axios.get(url);
   const html: string = response.data;
 
   console.log(Scraper.getPlaylist(html));
-})();
+});
---
+++ src/auth/auth.ts
@@ -0,0 +1,107 @@
+import express from 'express';
+import querystring from 'querystring';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import axios from 'axios';
+
+interface AuthTokens {
+  accessToken: string
+  refreshToken: string
+}
+
+function generateRandomString(length: number): string {
+  const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let text = '';
+
+  for (let i = 0; i < length; i += 1) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+
+  return text;
+}
+
+// eslint-disable-next-line no-unused-vars
+type CallbackType = (t: AuthTokens) => void;
+
+function auth(clientId: string, clientSecret: string, port: string, callback: CallbackType): void {
+  const stateKey = 'spotify_auth_state';
+
+  const app = express();
+  app.use(cors()).use(cookieParser());
+  const server = app.listen(process.env.PORT, () => {
+    console.log(`Access http://localhost:${port}/login and log in to your Spotify account.`);
+  });
+
+  app.get('/login', (req, res) => {
+    const state = generateRandomString(16);
+    res.cookie(stateKey, state);
+
+    const scope = 'user-read-private user-read-email';
+    const redirectUri = `http://localhost:${port}/callback`;
+
+    const query = querystring.stringify({
+      response_type: 'code',
+      client_id: clientId,
+      redirect_uri: redirectUri,
+      scope,
+      state,
+    });
+    const authUrl = `https://accounts.spotify.com/authorize?${query}`;
+
+    res.redirect(authUrl);
+  });
+
+  app.get('/callback', async (req, res) => {
+    const code = req.query.code || null;
+    const state = req.query.state || null;
+    const storedState = req.cookies ? req.cookies[stateKey] : null;
+
+    if (state === null || state !== storedState) {
+      res.redirect('/fail');
+      return;
+    }
+
+    res.clearCookie(stateKey);
+    const redirectUri = `http://localhost:${port}/callback`;
+
+    try {
+      const response = await axios({
+        method: 'post',
+        url: 'https://accounts.spotify.com/api/token',
+        data: querystring.stringify({
+          grant_type: 'authorization_code',
+          redirect_uri: redirectUri,
+          code: `${code}`,
+        }),
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          Authorization: `Basic ${Buffer.from(`${clientId}:${clientSecret}`).toString('base64')}`,
+        },
+      });
+
+      const accessToken = response.data.access_token;
+      const refreshToken = response.data.refresh_token;
+
+      res.redirect('/success');
+
+      callback({
+        accessToken,
+        refreshToken,
+      });
+    } catch (err) {
+      console.log(err);
+    } finally {
+      server.close();
+    }
+  });
+
+  app.get('/success', (req, res) => {
+    res.send('Logged in successfuly!');
+  });
+
+  app.get('/fail', (req, res) => {
+    res.send('Failed to log in!');
+  });
+}
+
+export { AuthTokens, auth };
